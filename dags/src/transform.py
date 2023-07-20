c86b8703cb2e874ae42fbc6ea134eaad7acb0a82
wangcong <38272856+wangcong91@users.noreply.github.com>
2023-07-21 02:20:27 +0800

    add transform erp gantry function

--- dags/src/transform.py
+++ dags/src/transform.py
@@ -1,8 +1,7 @@
-from .utils import load_env, download_latest_file_from_s3, upload_file_to_s3
+from .utils import load_env, download_latest_file_from_s3, upload_file_to_s3,read_csvfile_from_s3
 import pandas as pd
 import json
 import os
-
 
 def transform_lta_bus():
     RAW_BUCKET = 'raw-kungfu-challenge'
@@ -18,5 +17,38 @@ def transform_lta_bus():
     df.to_csv(os.path.join('transformed_data','transformed.csv'))
 
 
+def transform_lta_erp_rate():
+
+    DEV = False
+    RAW_BUCKET = 'raw-kungfu-challenge'
+    PRESENTATION_BUCKET = 'presentation-kungfu-challenge'
+    PREFIX_ERP = 'erp_rate'
+
+    erp_zone_object_key = f'{PREFIX_ERP}/gantry_zone/erp_zone_location.csv'
+
+    if DEV == True:
+        erp_zone_file_path = os.path.join('../data','erp_zone_location.csv')
+        zone_data = pd.read_csv(erp_zone_file_path,header = 1)
+        file_path = os.path.join('./data','data_20230720_235013.json')
+
+    else:
+        zone_data = read_csvfile_from_s3(RAW_BUCKET,erp_zone_object_key)
+        file_path = download_latest_file_from_s3(RAW_BUCKET, PREFIX_ERP)
+
+
+
+    raw_df = pd.read_json(file_path)
+    erp_df = pd.json_normalize(raw_df['value'])
+    combined_df = erp_df.merge(zone_data,left_on= 'ZoneID',right_on = 'Zone ID')
+
+    temp_data_file_path = os.path.join('./data','erp_gantry_data.csv')
+    erp_presentation = combined_df.to_csv(temp_data_file_path,index = False)
+
+    upload_file_to_s3(PRESENTATION_BUCKET, PREFIX_ERP, temp_data_file_path)
+
+    os.remove(temp_data_file_path)
+    print(f"{temp_data_file_path} removed from local")
+
 #Main
 load_env()
+
